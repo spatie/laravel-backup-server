47cde5e78688a5f408528c7e37ac4c13e3848497
freek <freek@spatie.be>
2020-03-30 19:59:24 +0200

    wip

--- src/Models/Concerns/HasAsyncDelete.php
+++ src/Models/Concerns/HasAsyncDelete.php
@@ -14,5 +14,10 @@ trait HasAsyncDelete
         dispatch(new $deletionJobClassName($this));
     }
 
+    public function willBeDeleted(): bool
+    {
+        return $this->status === static::STATUS_DELETING;
+    }
+
     abstract public function getDeletionJobClassName(): string;
 }
