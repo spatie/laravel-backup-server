a7493097d87619bda4cd8b7398ee2ce2a316e939
Alex Vanderbist <alex.vanderbist@gmail.com>
2020-04-22 18:33:17 +0200

    Notification copy

--- resources/lang/en/notifications.php
+++ resources/lang/en/notifications.php
@@ -13,15 +13,15 @@ return [
     'backup_completed_body' => 'Great news, a new backup of :source_name was successfully created on :destination_name.',
 
     'cleanup_source_successful_subject' => 'Clean up of :source_name backups successful',
-    'cleanup_source_successful_subject_title' => 'Successful clean up!',
-    'cleanup_source_successful_body' => 'The clean up of the :source_name backups.',
+    'cleanup_source_successful_subject_title' => 'All clean!',
+    'cleanup_source_successful_body' => 'The backups of :source_name were succesfully cleaned up.',
 
     'cleanup_source_failed_subject' => 'Clean up of :source_name backups failed',
     'cleanup_source_failed_subject_title' => 'Clean up failed!',
-    'cleanup_source_failed_body' => 'An error occurred while cleaning up the backups of :destination_name.',
+    'cleanup_source_failed_body' => 'An error occurred while cleaning up the backups of :source_name.',
 
     'cleanup_destination_successful_subject' => 'Clean up of backups on :destination_name succesfull',
-    'cleanup_destination_successful_subject_title' => 'Successful clean up!',
+    'cleanup_destination_successful_subject_title' => 'All clean!',
     'cleanup_destination_successful_body' => 'The backups on :destination_name were successfully cleaned up.',
 
     'cleanup_destination_failed_subject' => 'Clean up of :destination_name backups failed',
@@ -30,17 +30,17 @@ return [
 
     'healthy_source_found_subject' => 'The backups for :source_name are healthy',
     'healthy_source_found_subject_title' => 'Healthy backups',
-    'healthy_source_found_body' => 'The backups for :source_name are considered healthy.',
+    'healthy_source_found_body' => 'A health check ran and the backups for :source_name are considered healthy.',
 
     'unhealthy_source_found_subject' => 'Important: The backups for :source_name are unhealthy',
     'unhealthy_source_found_subject_title' => 'Unhealthy backups',
-    'unhealthy_source_found_body' => 'Important: The backups for :source_name are unhealthy.',
+    'unhealthy_source_found_body' => 'Important: A health check ran and the backups for :source_name are unhealthy.',
 
     'healthy_destination_found_subject' => 'The backup destination :destination_name is healthy',
     'healthy_destination_found_subject_title' => 'Healthy backup destination',
-    'healthy_destination_found_body' => 'The backup destination :destination_name is considered healthy.',
+    'healthy_destination_found_body' => 'A health check ran and the backup destination :destination_name is considered healthy.',
 
     'unhealthy_destination_found_subject' => 'Important: The backup destination :destination_name is unhealthy',
     'unhealthy_destination_found_subject_title' => 'Unhealthy backup destination',
-    'unhealthy_destination_found_body' => 'The backup destination :destination_name is unhealthy.',
+    'unhealthy_destination_found_body' => 'A health check ran and the backup destination :destination_name is unhealthy.',
 ];
