5382d6cae8ada42cca3b7602a5761429b9534104
Niels <niels@spatie.be>
2020-10-22 09:56:08 +0200

    add ability to sort sources in command

--- src/Commands/ListSourcesCommand.php
+++ src/Commands/ListSourcesCommand.php
@@ -8,15 +8,22 @@ use Spatie\BackupServer\Models\Source;
 use Spatie\BackupServer\Support\AlignCenterTableStyle;
 use Spatie\BackupServer\Support\AlignRightTableStyle;
 use Spatie\BackupServer\Support\Helpers\Format;
+use Spatie\BackupServer\Tasks\Backup\Support\BackupCollection;
 
 class ListSourcesCommand extends Command
 {
-    protected $name = 'backup-server:list';
+    protected $signature = 'backup-server:list {--sortBy=}';
 
     protected $description = 'Display a list of the last backup of all sources';
 
+    protected array $allowedArgumentValues = ['name', 'healthy', 'created_at'];
+
     public function handle()
     {
+        $sortBy = (string) ($this->option('sortBy') ?? 'name');
+
+        $this->guardAgainstInvalidOptionValues($sortBy);
+
         $headers = [
             'Source',
             'Id',
@@ -29,7 +36,7 @@ class ListSourcesCommand extends Command
         ];
 
         $rows = Source::get()
-            ->sortBy(fn (Source $source) => $source->name)
+            ->sortBy(fn (Source $source) => $source->getAttribute($sortBy))
             ->map(fn (Source $source) => $this->convertToRow($source));
 
         $columnStyles = collect($headers)
@@ -50,8 +57,9 @@ class ListSourcesCommand extends Command
         $this->table($headers, $rows, 'default', $columnStyles);
     }
 
-    protected function convertToRow(Source $source)
+    protected function convertToRow(Source $source): array
     {
+        /** @var BackupCollection $completedBackups */
         $completedBackups = $source->completedBackups;
 
         if ($source->completedBackups->isEmpty()) {
@@ -86,5 +94,12 @@ class ListSourcesCommand extends Command
         return is_null($backup)
             ? 'No backups present'
             : Format::ageInDays($backup->created_at);
+    }
+
+    public function guardAgainstInvalidOptionValues(string $optionValue): void
+    {
+        if (! in_array($optionValue, $this->allowedArgumentValues, true)) {
+            throw new \RuntimeException("Use one of these values: " . implode(', ', $this->allowedArgumentValues));
+        }
     }
 }
