ac57e9ea05cb480d81a9b9b514595e1f35123894
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2024-06-12 14:00:41 +0200

    diferentiate between status enum types

--- src/Models/Concerns/HasAsyncDelete.php
+++ src/Models/Concerns/HasAsyncDelete.php
@@ -3,12 +3,16 @@
 namespace Spatie\BackupServer\Models\Concerns;
 
 use Spatie\BackupServer\Enums\BackupStatus;
+use Spatie\BackupServer\Enums\SourceStatus;
+use Spatie\BackupServer\Models\Backup;
+use Spatie\BackupServer\Models\Destination;
+use Spatie\BackupServer\Models\Source;
 
 trait HasAsyncDelete
 {
     public function asyncDelete(): void
     {
-        $this->update(['status' => BackupStatus::Deleting]);
+        $this->update(['status' => $this->status($this)]);
 
         $deletionJobClassName = $this->getDeletionJobClassName();
 
@@ -17,8 +21,18 @@ trait HasAsyncDelete
 
     public function willBeDeleted(): bool
     {
-        return $this->status === BackupStatus::Deleting;
+        return $this->status === $this->status($this);
     }
 
     abstract public function getDeletionJobClassName(): string;
+
+    protected function status(self $class)
+    {
+        return match ($class::class) {
+            Source::class => SourceStatus::Deleting,
+            Destination::class => Destination::STATUS_DELETING,
+            Backup::class => BackupStatus::Deleting,
+            default => throw new \Exception('Unknown class type'),
+        };
+    }
 }
