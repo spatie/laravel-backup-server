80f66fe9ad8755f97f3ed54c37fe870e1ad4d880
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2024-12-31 13:57:22 +0100

    improve

--- src/Notifications/EventHandler.php
+++ src/Notifications/EventHandler.php
@@ -66,9 +66,9 @@ class EventHandler
     protected function shouldSendNotification(object $event): bool
     {
         return match (true) {
-            $event instanceof BackupCompletedEvent::class => ! $event->backup->source->hasFailedNotificationsPaused(),
-            $event instanceof BackupFailedEvent::class => ! $event->backup->source->hasFailedNotificationsPaused(),
-            $event instanceof UnhealthySourceFoundEvent::class => ! $event->source->hasFailedNotificationsPaused(),
+            $event instanceof BackupCompletedEvent => ! $event->backup->source->hasFailedNotificationsPaused(),
+            $event instanceof BackupFailedEvent => ! $event->backup->source->hasFailedNotificationsPaused(),
+            $event instanceof UnhealthySourceFoundEvent => ! $event->source->hasFailedNotificationsPaused(),
             default => true,
         };
     }
